4b5c9a3d493bb1b4c9bee00f325a36e123ed64cc
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-11 03:55:36 -0500

    chore: Update db sql

--- postgresql/app/db.sql
+++ postgresql/app/db.sql
@@ -1,6 +1,20 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  content TEXT NOT NULL,
+);
+
 DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   content TEXT NOT NULL,
+  postid INT NOT NULL,
+);
+
+DROP TABLE IF EXISTS reactions;
+CREATE TABLE reactions (
+  id SERIAL PRIMARY KEY,
+  emoji TEXT NOT NULL,
+  commentid INT NOT NULL,
   postid INT NOT NULL
 );
